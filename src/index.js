e52e2e31a53cb4e1fc77bec1b4663c23e955f909
Liu, Zhenya <liuzhenya@three-geeks.com>
2021-03-18 03:51:51 +0000

    引入 styled-components 管理项目样式; 引入 reset.css 的基础样式

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import { Fragment } from 'react'
 import ReactDOM from 'react-dom';
 import App from './App';
+import { GlobalStyle } from './style.js'
 
 ReactDOM.render(
-  <App />,
+  <Fragment>
+    <GlobalStyle />
+    <App />
+  </Fragment>,
   document.getElementById('root')
 )
